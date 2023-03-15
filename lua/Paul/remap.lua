966a6bebacfe7842aaa05f2bf411145b8a4b8c31
Paul <paul.kerrigan@ontariotechu.net>
2023-03-15 11:29:03 -0400

    some more config stuff

--- lua/Paul/remap.lua
+++ lua/Paul/remap.lua
@@ -1,3 +1,32 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("i", "jj", "<Esc>") 
+vim.g.mapleader = " ";
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex);
+vim.keymap.set("i", "jj", "<Esc>");
+vim.keymap.set("n", "<leader>dd", ":Lexplore %:p:h<CR>");
+vim.keymap.set("n", "<leader>da", ":Lexplore");
+local function netrw_mapping()
+  local bufmap = function(lhs, rhs)
+    local opts = {buffer = true, remap = true}
+    vim.keymap.set('n', lhs, rhs, opts)
+  end
+
+  -- close window
+  bufmap('<leader>dd', ':Lexplore<CR>')
+  bufmap('<leader>da', ':Lexplore<CR>')
+
+  -- Better navigation
+  --bufmap('H', 'u')
+  --bufmap('h', '-^')
+ -- bufmap('l', '<CR>')
+  --bufmap('L', '<CR>:Lexplore<CR>')
+
+  -- Toggle dotfiles
+  --bufmap('.', 'gh')
+end
+
+local user_cmds = vim.api.nvim_create_augroup('user_cmds', {clear = true})
+vim.api.nvim_create_autocmd('filetype', {
+  pattern = 'netrw',
+  group = user_cmds,
+  desc = 'Keybindings for netrw',
+  callback = netrw_mapping
+});
