332a672a99df3dce254f8e5d76b9ef03863d0b5a
Paul Kerrigan <pauljr4411@gmail.com>
2023-03-25 14:57:04 -0400

    tsx

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,7 +1,7 @@
 
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "help", "javascript", "typescript", "c", "lua", "rust", "go", "comment" },
+  ensure_installed = {"tsx", "help", "javascript", "typescript", "c", "lua", "rust", "go", "comment" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
